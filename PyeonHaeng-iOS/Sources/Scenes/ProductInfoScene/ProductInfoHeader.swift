533741ea6a7a084e96551b96c3fda3e7fdb904f1
EungCheol Kim <wannabeeung@gmail.com>
2024-01-26 14:41:34 +0900

    💄 ProductInfoHeader UI 파일 추가
    해당 파일은 제품 상세 페이지 최상단에 있는 HeaderView입니다.

---
+++ PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoHeader.swift
@@ -0,0 +1,18 @@
+//
+//  ProductInfoHeader.swift
+//  PyeonHaeng-iOS
+//
+//  Created by 김응철 on 2024/1/26.
+//
+
+import SwiftUI
+
+struct ProductInfoHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductInfoHeader()
+}
