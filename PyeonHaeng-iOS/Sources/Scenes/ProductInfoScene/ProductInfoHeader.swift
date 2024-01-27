a1295e7683618107bd206c2a3439c5678483a7b6
EungCheol Kim <wannabeeung@gmail.com>
2024-01-27 09:36:48 +0900

    💄 ProductInfoHeader, 제품 이름, 가격, 할인 행사 종류 관련 UI 구현제외되어야할 항목
    현재 제외되는 항목
    – 제품의 카테고리 레이블
    – 제품의 원래 가격 레이블

--- PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoHeader.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoHeader.swift
@@ -11,6 +11,7 @@ struct ProductInfoHeader: View {
   
   enum MockProduct {
     static let imageURL: String = "https://image.woodongs.com/imgsvr/item/GD_8809288635315_003.jpg"
+    static let name: String = "펩시 제로 라임 250ml"
   }
   
   var body: some View {
@@ -26,6 +27,28 @@ struct ProductInfoHeader: View {
       .frame(maxWidth: .infinity, maxHeight: 257.0)
       .padding(.top, 44.12)
       .padding(.bottom, 40.45)
+      
+      Text(MockProduct.name)
+        .textStyle(.h3)
+        .frame(maxWidth: .infinity, alignment: .leading)
+      
+      HStack(alignment: .bottom) {
+        VStack(alignment: .leading, spacing: .zero) {
+          Text("행사 진행 편의점")
+            .textStyle(.c2)
+            .padding(.top, 16.0)
+          Image(.GS_25)
+            .padding(.top, 2.0)
+        }
+        Spacer()
+        HStack(spacing: 8.0) {
+          Text("개당")
+            .textStyle(.c1)
+            .frame(maxHeight: 38.0)
+          Text("1,250원")
+            .textStyle(.h2)
+        }
+      }
     }
   }
 }
