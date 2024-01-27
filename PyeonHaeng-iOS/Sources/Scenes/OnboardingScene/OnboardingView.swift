f4a3d732fa4166f0f87b8cf6f30914e287941f3c
isGeekCode <h1guitar@naver.com>
2024-01-28 03:18:16 +0900

    :lipstick: 커스텀페이지컨트롤 세팅

--- PyeonHaeng-iOS/Sources/Scenes/OnboardingScene/OnboardingView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/OnboardingScene/OnboardingView.swift
@@ -16,7 +16,7 @@ struct OnboardingView: View {
     @State private var bodyText = """
                                    다양한 편의점의 1+1, 2+1 행사 제품 정보로
                                    알뜰하고 합리적으로 소비할수 있어요.
-                                   """
+                                  """
 
     var body: some View {
         NavigationView {
@@ -49,14 +49,19 @@ struct OnboardingView: View {
                     updateTexts(for: currentPage)
                 })
 
+                // 커스텀 페이지 컨트롤
+                CustomPageControl(currentPage: $currentPage, pageCount: 2)
+                    .padding(.vertical)
+
                 // 본문 제목
                 Text(titleText)
                     .textStyle(.h2)
-                    .padding(.top, 20)
+                Spacer().frame(height: 16)
 
                 // 본문 내용
                 Text(bodyText)
                     .textStyle(.body2)
+                    .foregroundStyle(.gray500)
                     .multilineTextAlignment(.center)
                     .padding(.top, 8)
 
@@ -87,11 +92,42 @@ struct OnboardingView: View {
     func updateTexts(for page: Int) {
         if page == 0 {
             titleText = "하나사면 하나더"
-            bodyText = "다양한 편의점의 1+1, 2+1 행사 제품 정보로\n알뜰하고 합리적으로 소비할수 있어요."
+            bodyText = """
+                       다양한 편의점의 1+1, 2+1 행사 제품 정보로
+                       알뜰하고 합리적으로 소비할수 있어요.
+                       """
         } else if page == 1 {
             titleText = "수많은 혜택을 한곳에서"
-            bodyText = "세븐일레븐, CU, 이마트 24, GS 25, 미니스톱의 수많은 행사정보를\n‘편행’한곳에서 만나보세요."
+            bodyText = """
+                       세븐일레븐, CU, 이마트 24, GS 25, 미니스톱의
+                       수많은 행사정보를 ‘편행’한곳에서 만나보세요.
+                       """
         }
+    }
+}
+
+struct CustomPageControl: View {
+    @Binding var currentPage: Int
+    var pageCount: Int
+    
+    var body: some View {
+        HStack {
+            ForEach(0..<pageCount, id: \.self) { index in
+                if currentPage == index {
+                    Rectangle()
+                        .frame(width: 24, height: 6)
+                        .foregroundColor(.main500)
+                        .transition(.scale)
+                } else {
+                    Circle()
+                        .frame(width: 6, height: 6)
+                        .foregroundColor(.gray)
+                        .transition(.scale)
+                }
+            }
+        }
+        // 페이지 변경 시 애니메이션 효과
+        .animation(.default, value: currentPage)
     }
 }
 
