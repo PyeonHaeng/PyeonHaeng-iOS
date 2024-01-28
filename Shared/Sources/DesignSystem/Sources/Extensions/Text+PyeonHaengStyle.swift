9e1dc84240b6886278d1b234cd5f2172d98a388c
EungCheol Kim <wannabeeung@gmail.com>
2024-01-28 14:10:02 +0900

    🔧 Text.foregroundColor() 오버로딩 메서드 구현

--- Shared/Sources/DesignSystem/Sources/Extensions/Text+PyeonHaengStyle.swift
+++ Shared/Sources/DesignSystem/Sources/Extensions/Text+PyeonHaengStyle.swift
@@ -84,4 +84,9 @@ public extension Text {
       font(.custom(Pretendard.regular.rawValue, size: 12))
     }
   }
+  
+  func foregroundColor(_ style: PyeonHaengColor) -> Text {
+    foregroundStyle(Color(style.rawValue))
+  }
+  
 }
