a03a2117019894e5ba403f9ac1c8465d81c499a8
EungCheol Kim <wannabeeung@gmail.com>
2024-01-28 14:07:53 +0900

    🔧 편행 앱에 사용되는 색상 enum 구현

---
+++ Shared/Sources/DesignSystem/Sources/PyeonHaengColor.swift
@@ -0,0 +1,39 @@
+//
+//  PyeonHaengColor.swift
+//
+//
+//  Created by 김응철 on 2024/1/28.
+//
+
+import Foundation
+
+public enum PyeonHaengColor: String {
+  case gray050
+  case gray100
+  case gray200
+  case gray300
+  case gray400
+  case gray500
+  case gray600
+  case gray700
+  case gray800
+  case gray900
+
+  case green05
+  case green10
+  case green20
+  case green30
+  case green40
+  case green50
+  case green60
+  case green70
+  case green80
+  case green90
+
+  case pyeonHaengBlue
+  case pyeonHaengGreen
+  case pyeonHaengRed
+  case pyeonHaengYellow
+
+  case blur
+}
