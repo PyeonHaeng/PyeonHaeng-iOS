bbbca79bbe477734489ca8133af824b79292ef07
EungCheol Kim <wannabeeung@gmail.com>
2024-01-28 14:11:11 +0900

    🔧 모든 뷰에 전역적으로 사용할 수 오버로딩 메서드인 View+ 구현

---
+++ Shared/Sources/DesignSystem/Sources/Extensions/View+PyeonHaengStyle.swift
@@ -0,0 +1,18 @@
+//
+//  View+PyeonHaengStyle.swift
+//
+//
+//  Created by 김응철 on 2024/1/28.
+//
+
+import SwiftUI
+
+extension View {
+  func foregroundStyle(_ color: PyeonHaengColor) -> some View {
+    foregroundStyle(Color(color.rawValue))
+  }
+
+  func background(_ color: PyeonHaengColor) -> some View {
+    background(Color(color.rawValue))
+  }
+}
