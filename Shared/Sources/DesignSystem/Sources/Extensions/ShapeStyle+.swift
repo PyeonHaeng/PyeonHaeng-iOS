691a31735d40b2c1806ba1029238460d000f95c8
EungCheol Kim <wannabeeung@gmail.com>
2024-01-29 06:11:26 +0900

    ✨ ShapeStyle Extension 추가

---
+++ Shared/Sources/DesignSystem/Sources/Extensions/ShapeStyle+.swift
@@ -0,0 +1,39 @@
+//
+//  ShapeStyle+.swift
+//
+//
+//  Created by 김응철 on 2024/1/29.
+//
+
+import SwiftUI
+
+public extension ShapeStyle where Self == Color {
+  static var gray050: Color { .init(.gray050) }
+  static var gray100: Color { .init(.gray100) }
+  static var gray200: Color { .init(.gray200) }
+  static var gray300: Color { .init(.gray300) }
+  static var gray400: Color { .init(.gray400) }
+  static var gray500: Color { .init(.gray500) }
+  static var gray600: Color { .init(.gray600) }
+  static var gray700: Color { .init(.gray700) }
+  static var gray800: Color { .init(.gray800) }
+  static var gray900: Color { .init(.gray900) }
+
+  static var green050: Color { .init(.green050) }
+  static var green100: Color { .init(.green100) }
+  static var green200: Color { .init(.green200) }
+  static var green300: Color { .init(.green300) }
+  static var green400: Color { .init(.green400) }
+  static var green500: Color { .init(.green500) }
+  static var green600: Color { .init(.green600) }
+  static var green700: Color { .init(.green700) }
+  static var green800: Color { .init(.green800) }
+  static var green900: Color { .init(.green900) }
+
+  static var pyeonHaengBlue: Color { .init(.pyeonHaengBlue) }
+  static var pyeonHaengGreen: Color { .init(.pyeonHaengGreen) }
+  static var pyeonHaengRed: Color { .init(.pyeonHaengRed) }
+  static var pyeonHaengYellow: Color { .init(.pyeonHaengYellow) }
+  
+  static var blur: Color { .init(.blur) }
+}
