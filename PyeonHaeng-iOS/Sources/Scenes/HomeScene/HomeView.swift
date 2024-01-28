140d3b848d2aeb5a9360fdba728f7f15559729a3
SeungHyun Hong <whi7ehyun@gmail.com>
2024-01-28 11:15:31 +0900

    :lipstick: Implement Navigation Bar design on Home Screen

--- PyeonHaeng-iOS/Sources/Scenes/HomeScene/HomeView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/HomeScene/HomeView.swift
@@ -5,9 +5,59 @@
 //  Created by 홍승현 on 1/24/24.
 //
 
+import DesignSystem
 import SwiftUI
 
+// MARK: - HomeView
+
 struct HomeView: View {
+  var body: some View {
+    NavigationStack {
+      VStack {
+        HomeProductDetailSelectionView()
+        HomeProductListView()
+      }
+      .navigationBarTitleDisplayMode(.inline)
+      .toolbar {
+        ToolbarItem(placement: .topBarLeading) {
+          HStack {
+            Image(systemName: "storefront")
+              .foregroundStyle(Color.gray900)
+          }
+        }
+
+        ToolbarItemGroup(placement: .topBarTrailing) {
+          NavigationLink {
+            ProductInfoView()
+              .toolbarRole(.editor)
+          } label: {
+            Image(systemName: "magnifyingglass")
+              .foregroundStyle(Color.gray900)
+          }
+          NavigationLink {
+            SettingsView()
+              .toolbarRole(.editor)
+          } label: {
+            Image(systemName: "gear")
+              .foregroundStyle(Color.gray900)
+          }
+        }
+      }
+    }
+  }
+}
+
+// MARK: - HomeProductDetailSelectionView
+
+struct HomeProductDetailSelectionView: View {
+  var body: some View {
+    Text("")
+  }
+}
+
+// MARK: - HomeProductListView
+
+struct HomeProductListView: View {
   var body: some View {
     Text("")
   }
