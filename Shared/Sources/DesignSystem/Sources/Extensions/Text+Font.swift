2c557bfb95b906844b717159a81453b637a18d63
SeungHyun Hong <whi7ehyun@gmail.com>
2024-01-30 13:47:45 +0900

    :art: Adjust line spacing and refactor fonts to match Figma

--- Shared/Sources/DesignSystem/Sources/Extensions/Text+Font.swift
+++ Shared/Sources/DesignSystem/Sources/Extensions/Text+Font.swift
@@ -7,50 +7,78 @@
 import SwiftUI
 
 public extension Text {
-  func font(_ style: PyeonHaengFont) -> Text {
-    switch style {
-    case .h1:
-      font(.custom(Pretendard.bold.rawValue, size: 32))
-    case .h2:
-      font(.custom(Pretendard.bold.rawValue, size: 28))
-    case .h3:
-      font(.custom(Pretendard.bold.rawValue, size: 24))
-    case .h4:
-      font(.custom(Pretendard.bold.rawValue, size: 20))
-    case .h5:
-      font(.custom(Pretendard.bold.rawValue, size: 18))
-    case .title1:
-      font(.custom(Pretendard.bold.rawValue, size: 18))
-    case .title2:
-      font(.custom(Pretendard.bold.rawValue, size: 16))
-    case .title3:
-      font(.custom(Pretendard.bold.rawValue, size: 14))
-    case .body1:
-      font(.custom(Pretendard.medium.rawValue, size: 18))
-    case .body2:
-      font(.custom(Pretendard.medium.rawValue, size: 16))
-    case .body3:
-      font(.custom(Pretendard.medium.rawValue, size: 14))
-    case .b1:
-      font(.custom(Pretendard.semiBold.rawValue, size: 16))
-    case .b2:
-      font(.custom(Pretendard.semiBold.rawValue, size: 14))
-    case .b3:
-      font(.custom(Pretendard.semiBold.rawValue, size: 12))
-    case .c1:
-      font(.custom(Pretendard.regular.rawValue, size: 16))
-        .tracking((-1.5 / 1000) * 16)
-    case .c2:
-      font(.custom(Pretendard.regular.rawValue, size: 14))
-        .tracking((-1.5 / 1000) * 14)
-    case .c3:
-      font(.custom(Pretendard.regular.rawValue, size: 12))
-        .tracking((-1.5 / 1000) * 12)
-    case .c4:
-      font(.custom(Pretendard.regular.rawValue, size: 10))
-        .tracking((-1.5 / 1000) * 10)
-    case .x2:
-      font(.custom(Pretendard.regular.rawValue, size: 12))
-    }
+  func font(_ style: PyeonHaengFont) -> some View {
+    let setting = fontSetting(for: style)
+    let uiFont = UIFont(name: setting.name, size: setting.size) ?? .systemFont(ofSize: setting.size)
+    return fontWithLineHeight(font: uiFont, lineHeight: setting.lineHeight)
+      .tracking(setting.trackingAmount)
+  }
+}
+
+/// 폰트 스타일에 따른 설정을 반환하는 함수
+private func fontSetting(for style: PyeonHaengFont) -> FontSetting {
+  switch style {
+  case .h1:
+    return FontSetting(fontName: .bold, size: 32, lineHeight: 40)
+  case .h2:
+    return FontSetting(fontName: .bold, size: 28, lineHeight: 38)
+  case .h3:
+    return FontSetting(fontName: .bold, size: 24, lineHeight: 36)
+  case .h4:
+    return FontSetting(fontName: .bold, size: 20, lineHeight: 32)
+  case .h5:
+    return FontSetting(fontName: .bold, size: 18, lineHeight: 28)
+  case .title1:
+    return FontSetting(fontName: .bold, size: 18, lineHeight: 28)
+  case .title2:
+    return FontSetting(fontName: .bold, size: 16, lineHeight: 24)
+  case .title3:
+    return FontSetting(fontName: .bold, size: 14, lineHeight: 22)
+  case .body1:
+    return FontSetting(fontName: .medium, size: 18, lineHeight: 28)
+  case .body2:
+    return FontSetting(fontName: .medium, size: 16, lineHeight: 24)
+  case .body3:
+    return FontSetting(fontName: .medium, size: 14, lineHeight: 22)
+  case .b1:
+    return FontSetting(fontName: .semiBold, size: 16, lineHeight: 24)
+  case .b2:
+    return FontSetting(fontName: .semiBold, size: 14, lineHeight: 22)
+  case .b3:
+    return FontSetting(fontName: .semiBold, size: 12, lineHeight: 18)
+  case .c1:
+    let tracking = (-1.5 / 1000) * 16
+    return FontSetting(fontName: .regular, size: 16, lineHeight: 24, tracking: tracking)
+  case .c2:
+    let tracking = (-1.5 / 1000) * 14
+    return FontSetting(fontName: .regular, size: 14, lineHeight: 22, tracking: tracking)
+  case .c3:
+    let tracking = (-1.5 / 1000) * 12
+    return FontSetting(fontName: .regular, size: 12, lineHeight: 18, tracking: tracking)
+  case .c4:
+    let tracking = (-1.5 / 1000) * 10
+    return FontSetting(fontName: .regular, size: 10, lineHeight: 16, tracking: tracking)
+  case .x2:
+    return FontSetting(fontName: .regular, size: 12, lineHeight: 16)
+  }
+}
+
+// MARK: - FontWithLineHeight
+
+private struct FontWithLineHeight: ViewModifier {
+  let font: UIFont
+  let lineHeight: CGFloat
+
+  func body(content: Content) -> some View {
+    content
+      .font(Font(font))
+      .lineSpacing(lineHeight - font.lineHeight)
+      .padding(.vertical, (lineHeight - font.lineHeight) / 2)
+  }
+}
+
+private extension View {
+  func fontWithLineHeight(font: UIFont, lineHeight: CGFloat) -> some View {
+    ModifiedContent(content: self, modifier: FontWithLineHeight(font: font, lineHeight: lineHeight))
   }
 }
