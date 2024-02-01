ea34a222d0a598c9b4146cc9fcb79bfd87a6fd75
EungCheol Kim <wannabeeung@gmail.com>
2024-02-01 19:58:17 +0900

    💄 PR 요청사항 수정
    - 중복 코드 수정
    - Text(verbatim:)으로 수정
    - 곧 deprecated되는 코드 수정

--- PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoLineGraphView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoLineGraphView.swift
@@ -13,10 +13,10 @@ import SwiftUI
 struct ProductInfoLineGraphView: View {
   /// 임시데이터입니다. 이 데이터는 곧 편의점 상품 데이터로 교체됩니다.
   private let prices: [Int] = [1000, 1200, 1050]
-
+  
   /// 가격과 날짜 정보가 표시되는 `Indicator`의 위치 값입니다.
   @State var offset: CGSize = .zero
-
+  
   var body: some View {
     GeometryReader { reader in
       // 뷰의 프레임 크기 정보
@@ -38,7 +38,7 @@ struct ProductInfoLineGraphView: View {
         let pathWidth = interval * CGFloat(index + 1)
         return CGPoint(x: pathWidth, y: pathHeight)
       }
-
+      
       ZStack {
         // 그래프를 그립니다.
         Path { path in
@@ -48,40 +48,39 @@ struct ProductInfoLineGraphView: View {
           path.addLine(to: endPoint)
         }
         .stroke(.green500, style: StrokeStyle(lineWidth: 2.0))
-
+        
         // 그래프 데이터 위 표시되는 동그라미
         SymbolShape(points: points)
           .fill(.green500)
           .stroke(.white, lineWidth: 1.0)
-
+        
         // 그라데이션 백그라운드 컬러
         LinearGradient(stops: [
           Gradient.Stop(color: .green500.opacity(0.1), location: 0.00),
           Gradient.Stop(color: .green500.opacity(0), location: 1.00),
-          Gradient.Stop(color: .green500.opacity(0), location: 1.00),
         ], startPoint: UnitPoint(x: 0.5, y: 0), endPoint: UnitPoint(x: 0.5, y: 1))
-          .clipShape(
-            Path { path in
-              path.move(to: CGPoint(x: 0, y: height))
-              path.addLine(to: startPoint); path.addLine(to: points.first!)
-              path.addLines(points)
-              path.addLine(to: endPoint)
-              path.addLine(to: CGPoint(x: width, y: height))
-              path.addLine(to: CGPoint(x: 0, y: height))
-            }
-          )
+        .clipShape(
+          Path { path in
+            path.move(to: CGPoint(x: 0, y: height))
+            path.addLine(to: startPoint); path.addLine(to: points.first!)
+            path.addLines(points)
+            path.addLine(to: endPoint)
+            path.addLine(to: CGPoint(x: width, y: height))
+            path.addLine(to: CGPoint(x: 0, y: height))
+          }
+        )
       }
       .gesture(DragGesture().onChanged { value in
         let translation = value.location.x
         let index = max(min(Int((translation / interval).rounded() - 1), prices.count - 1), 0)
         offset = CGSize(width: points[index].x - 27.5, height: 0)
       })
-      .overlay(
+      .overlay(alignment: .bottomLeading) {
         VStack(spacing: 0) {
-          Text("2023.11")
+          Text(verbatim: "2023.11")
             .font(.c4)
             .foregroundStyle(.gray400)
-          Text("1,250원")
+          Text(verbatim: "1,250원")
             .font(.b1)
             .foregroundStyle(.gray900)
           Rectangle()
@@ -89,9 +88,8 @@ struct ProductInfoLineGraphView: View {
             .frame(width: 1.0, height: 122.0)
         }
         .frame(width: 55.0, height: 162.0)
-        .offset(offset == .zero ? CGSize(width: points.last!.x - 27.5, height: 0) : offset),
-        alignment: .bottomLeading
-      )
+        .offset(offset == .zero ? CGSize(width: points.last!.x - 27.5, height: 0) : offset)
+      }
     }
     .frame(height: 162.0)
   }
@@ -101,7 +99,7 @@ struct ProductInfoLineGraphView: View {
 
 struct SymbolShape: Shape {
   let points: [CGPoint]
-
+  
   func path(in _: CGRect) -> Path {
     var path = Path()
     for point in points {
