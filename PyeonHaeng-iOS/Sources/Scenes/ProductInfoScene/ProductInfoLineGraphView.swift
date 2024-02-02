5eea18e4207d727b34f05a04b6d9e621b59b7f00
EungCheol Kim <wannabeeung@gmail.com>
2024-02-02 19:16:47 +0900

    💄 요청사항 수정
    - 옵셔널 바인딩 되지 않은 곳들을 수정했습니다.
    - 코드를 정리했습니다.

--- PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoLineGraphView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoLineGraphView.swift
@@ -5,6 +5,7 @@
 //  Created by 김응철 on 1/31/24.
 //
 
+import Combine
 import DesignSystem
 import SwiftUI
 
@@ -14,39 +15,74 @@ struct ProductInfoLineGraphView: View {
   // MARK: - Properties
 
   /// 임시데이터입니다. 이 데이터는 곧 편의점 상품 데이터로 교체됩니다.
-  @State var prices: [Int] = [1000, 1200, 1050]
+  @State var prices: [Int]
 
-  @State private var width: CGFloat = .zero
-  @State private var height: CGFloat = .zero
   @State private var offset: CGSize = .zero
-
-  private var interval: CGFloat {
-    width / CGFloat(prices.count + 1)
-  }
-
-  private var points: [CGPoint] {
-    calculatePoints()
-  }
 
   // MARK: - View
 
   var body: some View {
     GeometryReader { reader in
+      let size = reader.size
+      let interval = size.width / CGFloat(prices.count + 1)
+      let points = calculatePoints(interval: interval, width: size.width)
+
       ZStack {
-        lineGraph()
-        circleSymbols()
-        gradientBackground()
+        Path { $0.addLines(points) }
+          .stroke(.green500, style: StrokeStyle(lineWidth: 2.0))
+
+        Path { path in
+          for point in points.dropFirst().dropLast() {
+            var point = point
+            point.x -= Metrics.symbolWidth / 2
+            point.y -= Metrics.symbolWidth / 2
+            path.addEllipse(in: CGRect(
+              origin: point,
+              size: CGSize(width: Metrics.symbolWidth, height: Metrics.symbolWidth)
+            ))
+          }
+        }
+        .fill(.green500)
+        .stroke(.white, lineWidth: 1.0)
+
+        LinearGradient(stops: [
+          Gradient.Stop(color: .green500.opacity(0.1), location: 0.0),
+          Gradient.Stop(color: .green500.opacity(0), location: 1.0),
+        ], startPoint: UnitPoint(x: 0.5, y: 0), endPoint: UnitPoint(x: 0.5, y: 1))
+          .clipShape(
+            Path { path in
+              path.move(to: .zero)
+              path.addLines(points)
+              path.addLine(to: CGPoint(x: size.width, y: size.height))
+              path.addLine(to: CGPoint(x: .zero, y: size.height))
+            }
+          )
       }
       .gesture(DragGesture().onChanged { value in
-        calculateOffset(for: value.location.x)
+        let index = max(min(Int((value.location.x / interval).rounded() - 1), prices.count - 1), 0)
+        offset = CGSize(width: points[index + 1].x - (Metrics.panelWidth / 2), height: 0)
       })
       .overlay(alignment: .bottomLeading) {
-        productPanel()
+        VStack(spacing: 0) {
+          Text(verbatim: "2023.11")
+            .font(.c4)
+            .foregroundStyle(.gray400)
+          Text(verbatim: "1,250원")
+            .font(.b1)
+            .foregroundStyle(.gray900)
+          Rectangle()
+            .foregroundStyle(.gray100)
+            .frame(width: 1.0, height: Metrics.panelPoleHeight)
+        }
+        .frame(width: Metrics.panelWidth, height: Metrics.panelHeight)
+        .offset(offset)
       }
       .onAppear {
-        width = reader.size.width
-        height = reader.size.height
-        calculateOffset(for: width)
+        if let lastPoint = points.dropLast().last {
+          offset = CGSize(width: lastPoint.x - (Metrics.panelWidth / 2), height: 0)
+        } else {
+          // TODO: 이전 행사 내역이 하나도 없을 때, UI를 숨길 것인지, 아니면 다른 조치 방법이 있는지?
+        }
       }
     }
     .frame(height: Metrics.frameHeight)
@@ -56,12 +92,15 @@ struct ProductInfoLineGraphView: View {
 // MARK: Helpers
 
 private extension ProductInfoLineGraphView {
-  func calculatePoints() -> [CGPoint] {
+  func calculatePoints(interval: CGFloat, width: CGFloat) -> [CGPoint] {
     var points = [CGPoint]()
     points.append(CGPoint(x: .zero, y: Metrics.maxHeight))
 
     for (index, price) in prices.enumerated() {
-      let progress = calculateProgress(for: price)
+      var progress: CGFloat = 1.0
+      if let maxPrice = prices.max(), maxPrice != 0 {
+        progress *= (CGFloat(price) / CGFloat(maxPrice)) == 1 ? 1.0 : 0.8
+      }
       let pathHeight = Metrics.maxHeight + ((1 - progress) * Metrics.maxHeight)
       let pathWidth = interval * CGFloat(index + 1)
       points.append(CGPoint(x: pathWidth, y: pathHeight))
@@ -69,74 +108,6 @@ private extension ProductInfoLineGraphView {
 
     points.append(CGPoint(x: width, y: Metrics.maxHeight))
     return points
-  }
-
-  func calculateProgress(for price: Int) -> CGFloat {
-    var progress = (CGFloat(price) / CGFloat(prices.max() ?? 0))
-    progress *= progress == 1 ? 1.0 : 0.8
-    return progress
-  }
-
-  func calculateOffset(for location: CGFloat) {
-    let index = max(min(Int((location / interval).rounded() - 1), prices.count - 1), 0)
-    offset = CGSize(width: points[index + 1].x - (Metrics.panelWidth / 2), height: 0)
-  }
-}
-
-// MARK: UI Helpers
-
-private extension ProductInfoLineGraphView {
-  func lineGraph() -> some View {
-    Path { $0.addLines(points) }
-      .stroke(.green500, style: StrokeStyle(lineWidth: 2.0))
-  }
-
-  func circleSymbols() -> some View {
-    Path { path in
-      for point in points.dropFirst().dropLast() {
-        var point = point
-        point.x -= Metrics.symbolWidth / 2
-        point.y -= Metrics.symbolWidth / 2
-        path.addEllipse(in: CGRect(
-          origin: point,
-          size: CGSize(width: Metrics.symbolWidth, height: Metrics.symbolWidth)
-        ))
-      }
-    }
-    .fill(.green500)
-    .stroke(.white, lineWidth: 1.0)
-  }
-
-  func gradientBackground() -> some View {
-    LinearGradient(stops: [
-      Gradient.Stop(color: .green500.opacity(0.1), location: 0.0),
-      Gradient.Stop(color: .green500.opacity(0), location: 1.0),
-    ], startPoint: UnitPoint(x: 0.5, y: 0), endPoint: UnitPoint(x: 0.5, y: 1))
-      .clipShape(
-        Path { path in
-          path.addLines(points)
-          path.addLine(to: CGPoint(x: width, y: height))
-          path.addLine(to: CGPoint(x: 0, y: height))
-          path.addLine(to: CGPoint.zero)
-          path.addLine(to: points.first!)
-        }
-      )
-  }
-
-  func productPanel() -> some View {
-    VStack(spacing: 0) {
-      Text(verbatim: "2023.11")
-        .font(.c4)
-        .foregroundStyle(.gray400)
-      Text(verbatim: "1,250원")
-        .font(.b1)
-        .foregroundStyle(.gray900)
-      Rectangle()
-        .foregroundStyle(.gray100)
-        .frame(width: 1.0, height: Metrics.panelPoleHeight)
-    }
-    .frame(width: Metrics.panelWidth, height: Metrics.panelHeight)
-    .offset(offset)
   }
 }
 
@@ -155,5 +126,5 @@ private extension ProductInfoLineGraphView {
 }
 
 #Preview(traits: .sizeThatFitsLayout) {
-  ProductInfoLineGraphView()
+  ProductInfoLineGraphView(prices: [1000, 1200, 1300, 1150])
 }
