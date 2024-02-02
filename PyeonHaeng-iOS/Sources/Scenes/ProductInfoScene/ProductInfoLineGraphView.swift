97b918210430ac6cb68db373f1be2ebb14b700b5
EungCheol Kim <wannabeeung@gmail.com>
2024-02-02 13:23:41 +0900

    💄 요청 수정사항 반영 & 코드 정리
    - Metrics enum 구현
    - 프로퍼티 전역적으로 빼서 어떤 View에도 접근 가능

--- PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoLineGraphView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoLineGraphView.swift
@@ -11,69 +11,73 @@ import SwiftUI
 // MARK: - ProductInfoLineGraphView
 
 struct ProductInfoLineGraphView: View {
+  // MARK: - Properties
+
   /// 임시데이터입니다. 이 데이터는 곧 편의점 상품 데이터로 교체됩니다.
-  private let prices: [Int] = [1000, 1200, 1050]
+  @State var prices: [Int] = [1000, 1200, 1050]
+
+  @State private var width: CGFloat = .zero
+  @State private var height: CGFloat = .zero
+  @State private var offset: CGSize = .zero
   
-  /// 가격과 날짜 정보가 표시되는 `Indicator`의 위치 값입니다.
-  @State var offset: CGSize = .zero
+  private var interval: CGFloat {
+    width / CGFloat(prices.count + 1)
+  }
+  
+  private var points: [CGPoint] {
+    var points = [CGPoint]()
+    points.append(CGPoint(x: .zero, y: Metrics.maxHeight))
+    for (index, price) in prices.enumerated() {
+      var progress = (CGFloat(price) / CGFloat(prices.max() ?? 0))
+      progress *= progress == 1 ? 1.0 : 0.8
+      let pathHeight = Metrics.maxHeight + (1 - progress) * Metrics.maxHeight
+      let pathWidth = interval * CGFloat(index + 1)
+      points.append(CGPoint(x: pathWidth, y: pathHeight))
+    }
+    points.append(CGPoint(x: width, y: Metrics.maxHeight))
+    return points
+  }
+  
+  // MARK: - View
   
   var body: some View {
     GeometryReader { reader in
-      // 뷰의 프레임 크기 정보
-      let height = reader.size.height
-      let width = reader.size.width
-      // 그래프가 올라올 수 있는 최대 높이
-      let maxHeight: CGFloat = 87.0
-      // 제일 높은 가격 데이터
-      let maxPrice: CGFloat = .init(prices.max() ?? 0)
-      // 가격 데이터간의 너비 간격
-      let interval = reader.size.width / CGFloat(prices.count + 1)
-      // 각 데이터의 위치 값을 나타내는 CGPoint
-      let startPoint = CGPoint(x: .zero, y: maxHeight)
-      let endPoint = CGPoint(x: width, y: maxHeight)
-      let points: [CGPoint] = prices.enumerated().map { index, price -> CGPoint in
-        var progress = (CGFloat(price) / maxPrice)
-        progress *= progress == 1 ? 1.0 : 0.8
-        let pathHeight = maxHeight + (1 - progress) * maxHeight
-        let pathWidth = interval * CGFloat(index + 1)
-        return CGPoint(x: pathWidth, y: pathHeight)
-      }
-      
       ZStack {
-        // 그래프를 그립니다.
+        Path { $0.addLines(points) }
+          .stroke(.green500, style: StrokeStyle(lineWidth: 2.0))
+        
         Path { path in
-          path.move(to: startPoint)
-          path.addLine(to: points.first!)
-          path.addLines(points)
-          path.addLine(to: endPoint)
+          points.dropFirst().dropLast().forEach { point in
+            var point = point
+            point.x -= Metrics.symbolWidth / 2
+            point.y -= Metrics.symbolWidth / 2
+            path.addEllipse(in: CGRect(
+              origin: point,
+              size: CGSize(width: Metrics.symbolWidth, height: Metrics.symbolWidth)
+            ))
+          }
         }
-        .stroke(.green500, style: StrokeStyle(lineWidth: 2.0))
+        .fill(.green500)
+        .stroke(.white, lineWidth: 1.0)
         
-        // 그래프 데이터 위 표시되는 동그라미
-        SymbolShape(points: points)
-          .fill(.green500)
-          .stroke(.white, lineWidth: 1.0)
-        
-        // 그라데이션 백그라운드 컬러
         LinearGradient(stops: [
-          Gradient.Stop(color: .green500.opacity(0.1), location: 0.00),
-          Gradient.Stop(color: .green500.opacity(0), location: 1.00),
+          Gradient.Stop(color: .green500.opacity(0.1), location: 0.0),
+          Gradient.Stop(color: .green500.opacity(0), location: 1.0),
         ], startPoint: UnitPoint(x: 0.5, y: 0), endPoint: UnitPoint(x: 0.5, y: 1))
         .clipShape(
           Path { path in
-            path.move(to: CGPoint(x: 0, y: height))
-            path.addLine(to: startPoint); path.addLine(to: points.first!)
             path.addLines(points)
-            path.addLine(to: endPoint)
             path.addLine(to: CGPoint(x: width, y: height))
             path.addLine(to: CGPoint(x: 0, y: height))
+            path.addLine(to: CGPoint.zero)
+            path.addLine(to: points.first!)
           }
         )
       }
       .gesture(DragGesture().onChanged { value in
         let translation = value.location.x
         let index = max(min(Int((translation / interval).rounded() - 1), prices.count - 1), 0)
-        offset = CGSize(width: points[index].x - 27.5, height: 0)
+        offset = CGSize(width: points[index].x - Metrics.panelWidth / 2, height: 0)
       })
       .overlay(alignment: .bottomLeading) {
         VStack(spacing: 0) {
@@ -85,29 +89,32 @@ struct ProductInfoLineGraphView: View {
             .foregroundStyle(.gray900)
           Rectangle()
             .foregroundStyle(.gray100)
-            .frame(width: 1.0, height: 122.0)
+            .frame(width: 1.0, height: Metrics.panelPoleHeight)
         }
-        .frame(width: 55.0, height: 162.0)
-        .offset(offset == .zero ? CGSize(width: points.last!.x - 27.5, height: 0) : offset)
+        .frame(width: Metrics.panelWidth, height: Metrics.panelHeight)
+        .offset(offset)
+      }
+      .onAppear {
+        width = reader.size.width
+        height = reader.size.height
+        offset = CGSize(width: points.dropLast().last!.x - Metrics.panelWidth / 2, height: 0)
       }
     }
-    .frame(height: 162.0)
+    .frame(height: Metrics.frameHeight)
   }
 }
 
-// MARK: - SymbolShape
+// MARK: ProductInfoLineGraphView.Metrics
 
-struct SymbolShape: Shape {
-  let points: [CGPoint]
-  
-  func path(in _: CGRect) -> Path {
-    var path = Path()
-    for point in points {
-      var point = point
-      point.x -= 2; point.y -= 2
-      path.addEllipse(in: CGRect(origin: point, size: CGSize(width: 4.0, height: 4.0)))
-    }
-    return path
+private extension ProductInfoLineGraphView {
+  enum Metrics {
+    static let maxHeight: CGFloat = 87.0
+    static let symbolWidth: CGFloat = 4.0
+    static let frameHeight: CGFloat = 162.0
+    
+    static let panelWidth: CGFloat = 55.0
+    static let panelHeight: CGFloat = 162.0
+    static let panelPoleHeight: CGFloat = 122.0
   }
 }
 
