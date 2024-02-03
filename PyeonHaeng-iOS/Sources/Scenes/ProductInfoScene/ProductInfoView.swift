fe3d028dd514f6f1bfb11d4759dd19522a657fcb
EungCheol Kim <wannabeeung@gmail.com>
2024-02-03 19:14:05 +0900

    💄 "이전행사내역" 섹션 추가

--- PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoView.swift
@@ -5,6 +5,7 @@
 //  Created by 홍승현 on 1/24/24.
 //
 
+import DesignSystem
 import SwiftUI
 
 // MARK: - ProductInfoView
@@ -12,10 +13,19 @@ import SwiftUI
 struct ProductInfoView: View {
   var body: some View {
     NavigationStack {
-      VStack(spacing: 8.0) {
+      VStack {
         ProductInfoHeader()
           .navigationTitle("제품 상세")
           .navigationBarTitleDisplayMode(.inline)
+
+        Text("이전 행사 내역")
+          .font(.title1)
+          .foregroundStyle(.gray900)
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .padding(.top, 8.0)
+        ProductInfoLineGraphView(prices: [1150, 1300, 1400, 1200])
+          .padding(.top, 4.0)
+
         Spacer()
       }
       .padding(.horizontal, 20.0)
