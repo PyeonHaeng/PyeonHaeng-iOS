8fcad2a0c79046523401316983e556fc604315c7
EungCheol Kim <wannabeeung@gmail.com>
2024-02-05 16:15:52 +0900

    🌱 ProductInfoPriceResponse DTO 구현

---
+++ APIService/Sources/ProductInfoAPI/Responses/ProductPriceResponse.swift
@@ -0,0 +1,26 @@
+//
+//  ProductPriceResponse.swift
+//
+//
+//  Created by 김응철 on 2/5/24.
+//
+
+import Foundation
+
+public struct ProductPriceResponse: Decodable {
+  let date: String
+  let price: Int
+  
+  enum CodingKeys: CodingKey {
+    case date
+    case price
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    // TODO: 서버에서 내려주는 날짜 형식으로 바꾸기
+    let dateString = try container.decode(Date.self, forKey: .date).formatted()
+    self.date = dateString
+    self.price = try container.decode(Int.self, forKey: .price)
+  }
+}
---
+++ APIService/Sources/ProductInfoAPISupport/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by 김응철 on 2/5/24.
+//
+
+import Foundation
