5bc066db1aaef8c7e2b3db69c0e591284a1a35b0
Hyeonseok Bang <76529148+isGeekCode@users.noreply.github.com>
2024-02-06 20:44:44 +0900

    :twisted_rightwards_arrows: Onboarding 페이지 UI구현 (#13)
    * :lipstick: 온보딩 페이지 UI구현
    
    * :lipstick: 탭뷰기능 추가
    
    * :lipstick: 커스텀페이지컨트롤 세팅
    
    * :art: OnboardingPage Enum 추가
    
    * :art: 코드포맷 수정
    
    * :bug: 온보딩 바디글 String 형식 수정
    
    * :lipstick: localizable파일 추가
    
    * :bug: swiftFormat 오류 수정
    
    * :boom: 변경된 메서드 수정
    
    * :boob: 빈칸제거 및 프로젝트 설정 변경
    
    * :art: 접근제어 수정
    
    * :bento: Onboarding 이미지 카탈로그 생성, enum 수정
    
    * :wastebasket: Deprecate 메서드 수정
    
    * :recycle: 문자열 변수 분리
    
    * :recycle: Spacer 높이 변수 분리
    
    * :dizzy: 페이지컨트롤 애니메이션 추가
    
    ㅎ
    
    * :zap: Corner radius 변경
    
    * :lipstick: PR 수정사항 반영
    
    * :lipstick: TopBarTrailing 수정
    
    * :art: ViewModel을 추가하여 state변수관리
    
    * :truck: Image+Resource 충돌로 인한 이미지 경로 이동
    
    해당 내용은 다음 PR에 반영 예정
    
    * :art: Onboarding View Model, Onboarding page type 분리
    
    * :art: OnboardingPageControl 분리
    
    * :art: OnboardingPageType에서 View요소 제거
    
    * :art: Enum  OnboardingPageType -> OnboardingPage 모델로 변경처리
    
    * :safety_vest: 접근제어 변경

---
+++ PyeonHaeng-iOS/Sources/Scenes/OnboardingScene/OnboardingPageControl.swift
@@ -0,0 +1,50 @@
+//
+//  OnboardingPageControl.swift
+//  PyeonHaeng-iOS
+//
+//  Created by 방현석 on 2/4/24.
+//
+
+import SwiftUI
+
+// MARK: - OnboardingPageControl
+
+struct OnboardingPageControl: View {
+  @Binding private var currentPage: Int
+  private var pageCount: Int
+
+  init(currentPage: Binding<Int>, pageCount: Int) {
+    _currentPage = currentPage
+    self.pageCount = pageCount
+  }
+
+  var body: some View {
+    HStack {
+      ForEach(0 ..< pageCount, id: \.self) { index in
+        Rectangle()
+          .frame(width: index == currentPage ? 24 : 6, height: 6)
+          .foregroundStyle(index == currentPage ? Color.green500 : Color.gray100)
+          .clipShape(.rect(cornerRadius: 3))
+          .onTapGesture {
+            withAnimation {
+              currentPage = index // 해당 인덱스로 페이지 변경
+            }
+          }
+      }
+    }
+    // 페이지 변경 시 애니메이션 효과
+    .animation(.default, value: currentPage)
+  }
+}
+
+// MARK: - CustomPageControl_Previews
+
+struct CustomPageControl_Previews: PreviewProvider {
+  @State static var currentPage = 0 // Preview를 위한 @State 변수
+
+  static var previews: some View {
+    OnboardingPageControl(currentPage: $currentPage, pageCount: 2)
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
--- PyeonHaeng-iOS/Sources/Scenes/OnboardingScene/OnboardingView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/OnboardingScene/OnboardingView.swift
@@ -7,9 +7,77 @@
 
 import SwiftUI
 
+// MARK: - OnboardingView
+
 struct OnboardingView: View {
+  @StateObject private var viewModel = OnboardingViewModel()
+
   var body: some View {
-    Text("")
+    NavigationStack {
+      VStack {
+        TabView(selection: $viewModel.currentPage) {
+          ForEach(0 ..< viewModel.pageCount, id: \.self) { index in
+            VStack {
+              Spacer().frame(height: viewModel.spacerHeight(for: index))
+              Image(viewModel.currentImageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.horizontal, 40)
+              Spacer()
+            }
+            .tag(index)
+          }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+
+        // 커스텀 페이지 컨트롤
+        OnboardingPageControl(currentPage: $viewModel.currentPage, pageCount: viewModel.pageCount)
+          .padding(.vertical)
+
+        // 본문 제목
+        Text(viewModel.currentTitle)
+          .font(.h2)
+          .transition(.opacity.combined(with: .slide))
+          .animation(.easeInOut, value: viewModel.currentPage)
+
+        Spacer().frame(height: 16)
+
+        // 본문 내용
+        Text(viewModel.currentBody)
+          .font(.body2)
+          .foregroundStyle(Color.gray500)
+          .multilineTextAlignment(.center)
+          .padding(.top, 8)
+          .transition(.opacity.combined(with: .slide))
+          .animation(.easeInOut, value: viewModel.currentPage)
+
+        Spacer().frame(height: 84)
+
+        Button(action: {
+          withAnimation {
+            viewModel.nextPage()
+          }
+        }) {
+          Text(viewModel.nextButtonText)
+            .font(.h5)
+            .foregroundStyle(.white)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.green500)
+            .clipShape(.rect(cornerRadius: 10))
+            .padding(.horizontal, 20)
+        }
+        .padding(.bottom, 8)
+      }
+      .toolbar {
+        ToolbarItem(placement: .topBarTrailing) {
+          Button(viewModel.skipButtonText) {
+            // 건너뛰기 버튼 액션
+          }
+          .foregroundStyle(Color.green500)
+        }
+      }
+    }
   }
 }
 
