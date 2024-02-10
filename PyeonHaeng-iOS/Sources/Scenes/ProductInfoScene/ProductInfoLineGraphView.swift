d358b2513d9736134c47b2e858c59859b19cefeb
EungCheol Kim <wannabeeung@gmail.com>
2024-02-10 14:47:02 +0900

    💄 LineGraph UI에 '이전 행사 정보' 레이블 UI 추가

--- PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoLineGraphView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoLineGraphView.swift
@@ -12,52 +12,58 @@ import SwiftUI
 
 struct ProductInfoLineGraphView: View {
   // MARK: - Properties
-
+  
   /// 임시데이터입니다. 이 데이터는 곧 편의점 상품 데이터로 교체됩니다.
   @State var prices: [Int]
-
+  
   @State private var offset: CGSize = .zero
   @State private var isFirstRender: Bool = false
-
+  
   // MARK: - View
-
+  
   var body: some View {
     GeometryReader { reader in
       let size = reader.size
       let interval = size.width / CGFloat(prices.count + 1)
       let points = calculatePoints(interval: interval, width: size.width)
-
-      ZStack {
-        Path { $0.addLines(points) }
-          .stroke(.green500, style: StrokeStyle(lineWidth: 2.0))
-
-        Path { path in
-          for point in points.dropFirst().dropLast() {
-            var point = point
-            point.x -= Metrics.symbolWidth / 2
-            point.y -= Metrics.symbolWidth / 2
-            path.addEllipse(in: CGRect(
-              origin: point,
-              size: CGSize(width: Metrics.symbolWidth, height: Metrics.symbolWidth)
-            ))
-          }
-        }
-        .fill(.green500)
-        .stroke(.white, lineWidth: 1.0)
-
-        LinearGradient(
-          colors: [.green500.opacity(0.1), .clear],
-          startPoint: .top,
-          endPoint: .bottom
-        )
-        .clipShape(
+      
+      VStack(spacing: 4.0) {
+        Text("이전 행사 정보")
+          .font(.title1)
+          .foregroundStyle(.gray900)
+          .frame(maxWidth: .infinity, alignment: .leading)
+        ZStack {
+          Path { $0.addLines(points) }
+            .stroke(.green500, style: StrokeStyle(lineWidth: 2.0))
+          
           Path { path in
-            path.move(to: .zero)
-            path.addLines(points)
-            path.addLine(to: CGPoint(x: size.width, y: size.height))
-            path.addLine(to: CGPoint(x: .zero, y: size.height))
+            for point in points.dropFirst().dropLast() {
+              var point = point
+              point.x -= Metrics.symbolWidth / 2
+              point.y -= Metrics.symbolWidth / 2
+              path.addEllipse(in: CGRect(
+                origin: point,
+                size: CGSize(width: Metrics.symbolWidth, height: Metrics.symbolWidth)
+              ))
+            }
           }
-        )
+          .fill(.green500)
+          .stroke(.white, lineWidth: 1.0)
+          
+          LinearGradient(
+            colors: [.green500.opacity(0.1), .clear],
+            startPoint: .top,
+            endPoint: .bottom
+          )
+          .clipShape(
+            Path { path in
+              path.move(to: .zero)
+              path.addLines(points)
+              path.addLine(to: CGPoint(x: size.width, y: size.height))
+              path.addLine(to: CGPoint(x: .zero, y: size.height))
+            }
+          )
+        }
       }
       .gesture(DragGesture().onChanged { value in
         let index = max(min(Int((value.location.x / interval).rounded() - 1), prices.count - 1), 0)
@@ -90,6 +96,7 @@ struct ProductInfoLineGraphView: View {
         }
       }
     }
+    .padding(EdgeInsets(top: 4.0, leading: .zero, bottom: 24.0, trailing: .zero))
     .frame(height: Metrics.frameHeight)
   }
 }
@@ -100,7 +107,7 @@ private extension ProductInfoLineGraphView {
   func calculatePoints(interval: CGFloat, width: CGFloat) -> [CGPoint] {
     var points = [CGPoint]()
     points.append(CGPoint(x: .zero, y: Metrics.lineMaxHeightFromTop))
-
+    
     if let maxPrice = prices.max(), maxPrice != 0 {
       for (index, price) in prices.enumerated() {
         let heightFactor = 1 - (CGFloat(price) / CGFloat(maxPrice))
@@ -109,7 +116,7 @@ private extension ProductInfoLineGraphView {
         points.append(CGPoint(x: pathWidth, y: pathHeight))
       }
     }
-
+    
     points.append(CGPoint(x: width, y: Metrics.lineMaxHeightFromTop))
     return points
   }
@@ -120,10 +127,11 @@ private extension ProductInfoLineGraphView {
 private extension ProductInfoLineGraphView {
   enum Metrics {
     static let lineMaxHeightFromTop: CGFloat = 87.0
-    static let lineMaxHeightFromBottom: CGFloat = frameHeight - lineMaxHeightFromTop
+    static let lineMaxHeightFromBottom: CGFloat = lineGraphHeight - lineMaxHeightFromTop
     static let symbolWidth: CGFloat = 4.0
-    static let frameHeight: CGFloat = 162.0
-
+    static let frameHeight: CGFloat = 226.0
+    static let lineGraphHeight: CGFloat = 162.0
+    
     static let panelWidth: CGFloat = 55.0
     static let panelHeight: CGFloat = 162.0
     static let panelPoleHeight: CGFloat = 122.0
