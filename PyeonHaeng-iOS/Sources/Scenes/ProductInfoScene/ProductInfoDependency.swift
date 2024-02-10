1fd4f0580aad72251cc9b49f2809c0721f67f188
EungCheol Kim <wannabeeung@gmail.com>
2024-02-10 20:19:57 +0900

    🌱 ProductInfoDependency 구현

--- PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoDependency.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoDependency.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Network
+import ProductInfoAPI
+import ProductInfoAPISupport
+
+// MARK: - ProductInfoDependency
+
+protocol ProductInfoDependency {
+  var productInfoService: ProductInfoServiceRepresentable { get }
+}
+
+// MARK: - ProductInfoComponent
+
+struct ProductInfoComponent: ProductInfoDependency {
+  let productInfoService: ProductInfoServiceRepresentable
+  let productID: Int
+
+  init(productID: Int) {
+    self.productID = productID
+    let productInfoNetworking: Networking = {
+      let configuration = URLSessionConfiguration.ephemeral
+      configuration.protocolClasses = [ProductInfoURLProtocol.self]
+      let provider = NetworkProvider(session: URLSession(configuration: configuration))
+      return provider
+    }()
+
+    productInfoService = ProductInfoService(productID: productID,
+                                            network: productInfoNetworking)
+  }
+}
