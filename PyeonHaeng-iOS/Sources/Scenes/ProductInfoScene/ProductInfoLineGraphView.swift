5ad2e543328c2b979d2d06af1dfd541a8c251dc7
EungCheol Kim <wannabeeung@gmail.com>
2024-02-24 20:14:34 +0900

    🏗️ ProductInfoLineGraphView UI 코드 관련 구조화

--- PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoLineGraphView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoLineGraphView.swift
@@ -10,12 +10,8 @@ import SwiftUI
 
 // MARK: - ProductInfoLineGraphView
 
-struct ProductInfoLineGraphView: View {
-  // MARK: - Properties
-
-  /// 임시데이터입니다. 이 데이터는 곧 편의점 상품 데이터로 교체됩니다.
-  @State var prices: [Int]
-
+struct ProductInfoLineGraphView<ViewModel>: View where ViewModel: ProductInfoViewModelRepresentable {
+  @EnvironmentObject private var viewModel: ViewModel
   @State private var isFirstRender: Bool = false
   @State private var offset: CGSize = .zero
 
@@ -24,9 +20,10 @@ struct ProductInfoLineGraphView: View {
   var body: some View {
     GeometryReader { reader in
       let size = reader.size
-      let interval = size.width / CGFloat(prices.count + 1)
+      let previousProductsCount = viewModel.state.previousProducts.count
+      let interval = size.width / CGFloat(previousProductsCount + 1)
       let points = calculatePoints(interval: interval, width: size.width)
-      
+
       VStack(spacing: Metrics.spacing) {
         TextView()
         ZStack {
@@ -34,19 +31,16 @@ struct ProductInfoLineGraphView: View {
           LineGraphSymbolView(points: points)
           BackgroundGradientView(points: points, size: size)
         }
-        .gesture(DragGesture().onChanged { value in
-          let index = max(min(Int((value.location.x / interval).rounded() - 1), prices.count - 1), 0)
-          offset = CGSize(width: points[index + 1].x - (Metrics.panelWidth / 2), height: 0)
-        })
-        .overlay(alignment: .bottomLeading) {
-          LineGraphPanelView()
-        }
-        .frame(width: Metrics.panelWidth, height: Metrics.panelHeight)
-        .offset(offset)
       }
-      .onAppear {
-        isFirstRender = true
-      }
+      .gesture(DragGesture().onChanged { value in
+        let index = max(min(
+          Int((value.location.x / interval).rounded() - 1),
+          previousProductsCount - 1
+        ), 0)
+        offset = CGSize(width: points[index + 1].x - (Metrics.panelWidth / 2), height: 0)
+      })
+      .overlay(alignment: .bottomLeading) { LineGraphPanelView(offset: offset) }
+      .onAppear { isFirstRender = true }
       .onChange(of: isFirstRender) {
         guard isFirstRender else { return }
         if let lastPoint = points.dropLast().last {
@@ -56,11 +50,17 @@ struct ProductInfoLineGraphView: View {
         }
       }
     }
-    .padding(EdgeInsets(top: 4.0, leading: .zero, bottom: 24.0, trailing: .zero))
+    .padding(EdgeInsets(
+      top: Metrics.paddingTop,
+      leading: .zero,
+      bottom: Metrics.paddingBottom,
+      trailing: .zero
+    ))
     .frame(height: Metrics.frameHeight)
   }
-  
+
   private func calculatePoints(interval: CGFloat, width: CGFloat) -> [CGPoint] {
+    let prices = viewModel.state.previousProducts.map(\.price)
     var points = [CGPoint]()
     points.append(CGPoint(x: .zero, y: Metrics.lineMaxHeightFromTop))
 
@@ -89,21 +89,23 @@ private struct TextView: View {
   }
 }
 
+// MARK: - LineGraphView
+
 private struct LineGraphView: View {
   let points: [CGPoint]
-  
+
   var body: some View {
-    Path { path in
-      path.addLines(points)
-    }
-    .stroke(.green500, style: StrokeStyle(lineWidth: 2.0))
+    Path { $0.addLines(points) }
+      .stroke(.green500, style: StrokeStyle(lineWidth: 2.0))
   }
 }
+
+// MARK: - LineGraphSymbolView
 
 private struct LineGraphSymbolView: View {
   let points: [CGPoint]
   @State var offset: CGSize = .zero
-  
+
   var body: some View {
     Path { path in
       for point in points.dropFirst().dropLast() {
@@ -121,7 +123,11 @@ private struct LineGraphSymbolView: View {
   }
 }
 
+// MARK: - LineGraphPanelView
+
 private struct LineGraphPanelView: View {
+  let offset: CGSize
+
   var body: some View {
     VStack(spacing: .zero) {
       Text(verbatim: "2023.11")
@@ -134,6 +140,8 @@ private struct LineGraphPanelView: View {
         .foregroundStyle(.gray100)
         .frame(width: 1.0, height: Metrics.panelPoleHeight)
     }
+    .frame(width: Metrics.panelWidth, height: Metrics.panelHeight)
+    .offset(offset)
   }
 }
 
@@ -165,6 +173,9 @@ private struct BackgroundGradientView: View {
 private enum Metrics {
   static let spacing: CGFloat = 4.0
 
+  static let paddingTop = 4.0
+  static let paddingBottom = 24.0
+
   static let lineMaxHeightFromTop: CGFloat = 87.0
   static let lineMaxHeightFromBottom: CGFloat = lineGraphHeight - lineMaxHeightFromTop
   static let symbolWidth: CGFloat = 4.0
@@ -174,8 +185,4 @@ private enum Metrics {
   static let panelWidth: CGFloat = 55.0
   static let panelHeight: CGFloat = 162.0
   static let panelPoleHeight: CGFloat = 122.0
-}
-
-#Preview(traits: .sizeThatFitsLayout) {
-  ProductInfoLineGraphView(prices: [1, 2])
 }
