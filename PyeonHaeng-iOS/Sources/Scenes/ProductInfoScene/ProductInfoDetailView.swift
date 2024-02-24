c97f09e576ef65cd3d26a82ec041fa9c9060d5dd
EungCheol Kim <wannabeeung@gmail.com>
2024-02-24 20:14:13 +0900

    ✨ ProductInfoDetailView 편의점 로고 Image 데이터 바인딩

--- PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoDetailView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductInfoScene/ProductInfoDetailView.swift
@@ -57,12 +57,12 @@ private struct DetailView: View {
       VStack(alignment: .leading, spacing: .zero) {
         Text("행사 진행 편의점")
           .font(.c2)
-          .padding(.top, 16.0)
-        Image._7Eleven
-          .padding(.top, 2.0)
+          .padding(.top, Metrics.textPaddingTop)
+        image(for: product.convenienceStore)
+          .padding(.top, Metrics.convenienceStoreLogoPaddingTop)
       }
       Spacer()
-      HStack(spacing: 8.0) {
+      HStack(spacing: Metrics.horizontalSpacing) {
         PromotionTagView(promotionTag: promotionTag(for: product.promotion))
         Text("개당")
           .font(.c1)
@@ -84,6 +84,16 @@ private struct DetailView: View {
       .twoPlus
     }
   }
+
+  func image(for convenienceStore: ConvenienceStore) -> Image {
+    switch convenienceStore {
+    case .cu: .cu
+    case .gs25: .gs25
+    case ._7Eleven: ._7Eleven
+    case .emart24: .emart24
+    case .ministop: .ministop
+    }
+  }
 }
 
 // MARK: - Metrics
@@ -92,4 +102,8 @@ private enum Metrics {
   static let imageHeight: CGFloat = 257.0
   static let imagePaddingTop: CGFloat = 44.0
   static let imagePaddingBottom: CGFloat = 40.0
+
+  static let textPaddingTop: CGFloat = 16.0
+  static let convenienceStoreLogoPaddingTop: CGFloat = 2.0
+  static let horizontalSpacing: CGFloat = 8.0
 }
