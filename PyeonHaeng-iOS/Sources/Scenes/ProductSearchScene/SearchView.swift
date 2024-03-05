37665c6ee11eae7755b156f00f7e18a9df36618f
eung7 <wannabeeung@gmail.com>
2024-03-05 20:59:32 +0900

    🔁 SearchView UI 구현 및 데이터 바인딩 (#82)
    * 🎨 테스트 json 컴포넌트 변경
    
    * ✨ ConvenienceStore에서 이미지 추출 계산 프로퍼티 추가
    
    * ✨ Entity Package iOS17이상 지원으로 변경
    
    * ✨ 한 섹션 당 아이템들을 표현해줄 딕셔너리로 모델을 정의
    
    * ✨ SearchListCardView 수정 / Padding값, 모델 값 주입
    
    * ✨ SearchView에 데이터 주입 및 레이아웃 수정
    
    * 🎨 Compile Source 수정
    
    * 🔥 Entity와 DesignSystem간의 의존성 삭제
    
    * 🔥 요청 사항 수정
    
    - padding(.zero) 제거
    - 주석 처리 제거

--- PyeonHaeng-iOS/Sources/Scenes/ProductSearchScene/SearchView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductSearchScene/SearchView.swift
@@ -6,6 +6,7 @@
 //
 
 import DesignSystem
+import Entity
 import SwiftUI
 
 // MARK: - SearchView
@@ -19,26 +20,42 @@ struct SearchView<ViewModel>: View where ViewModel: SearchViewModelRepresentable
 
   var body: some View {
     ScrollView {
-      LazyVStack {
-        Section {
-          SearchListCardView()
-        } header: {
-          SearchHeaderView()
+      LazyVStack(spacing: .zero) {
+        ForEach(Array(viewModel.state.products), id: \.key) { key, items in
+          Section {
+            ForEach(items) { item in
+              SearchListCardView(product: item)
+            }
+          } header: {
+            SearchHeaderView(
+              store: key,
+              productsCount: items.count
+            )
+            .padding(.horizontal, Metrics.horizontalPadding)
+            .padding(.top, Metrics.headerTopPadding)
+          } footer: {
+            Rectangle()
+              .foregroundStyle(.gray050)
+              .frame(maxWidth: .infinity, maxHeight: 10)
+          }
         }
       }
     }
+    .scrollIndicators(.hidden)
     .toolbar {
       ToolbarItem(placement: .principal) {
-        SearchTextField()
+        SearchTextField<ViewModel>()
       }
     }
+    .environmentObject(viewModel)
   }
 }
 
 // MARK: - SearchTextField
 
-private struct SearchTextField: View {
+private struct SearchTextField<ViewModel>: View where ViewModel: SearchViewModelRepresentable {
   @State private var textInput: String = ""
+  @EnvironmentObject private var viewModel: ViewModel
 
   var body: some View {
     ZStack {
@@ -55,6 +72,7 @@ private struct SearchTextField: View {
               lineWidth: Metrics.textFieldBorderWidth
             )
         }
+        .onSubmit { viewModel.trigger(.textChanged(textInput)) }
       Button(action: {
         textInput = ""
       }) {
@@ -70,13 +88,35 @@ private struct SearchTextField: View {
 // MARK: - SearchHeaderView
 
 private struct SearchHeaderView: View {
+  let store: ConvenienceStore
+  let productsCount: Int
+
   var body: some View {
     HStack(spacing: 8.0) {
-      Image._7Eleven
-      Text(verbatim: "3")
+      convenienceImageView()
+        .resizable()
+        .scaledToFit()
+        .frame(height: 32.0)
+      Text(verbatim: "\(productsCount)")
         .font(.title2)
+        .foregroundStyle(.green500)
     }
-    .frame(maxWidth: .infinity, alignment: .leading)
+    .frame(maxWidth: .infinity, alignment: .bottomLeading)
+  }
+
+  private func convenienceImageView() -> Image {
+    switch store {
+    case .cu:
+      .cu
+    case .gs25:
+      .gs25
+    case ._7Eleven:
+      ._7Eleven
+    case .emart24:
+      .emart24
+    case .ministop:
+      .ministop
+    }
   }
 }
 
@@ -91,6 +131,9 @@ private enum Metrics {
   static let textFieldHeight = 28.0
   static let textFieldBorderWidth = 1.0
   static let cornerRadius = 8.0
+
+  static let horizontalPadding = 20.0
+  static let headerTopPadding = 24.0
 
   static let removeButtonSize = 32.0
 }
