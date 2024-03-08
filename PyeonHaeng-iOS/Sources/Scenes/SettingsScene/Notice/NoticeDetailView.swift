96e21cb2f6812a2c8753a619adc9a221baa2425b
SeungHyun Hong <whi7ehyun@gmail.com>
2024-03-08 15:04:37 +0900

    :lipstick: Enable tapping on announcements with NavigationLink
    - Implemented a NavigationLink for announcements, making them tappable. Used a `ZStack` to set the NavigationLink's opacity to 0 and display a custom-designed Label in the background

---
+++ PyeonHaeng-iOS/Sources/Scenes/SettingsScene/Notice/NoticeDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  NoticeDetailView.swift
+//  PyeonHaeng-iOS
+//
+//  Created by 홍승현 on 3/8/24.
+//
+
+import SwiftUI
+
+struct NoticeDetailView: View {
+  var body: some View {
+    Text("")
+  }
+}
+
+#if DEBUG
+#Preview {
+    NoticeDetailView()
+}
+#endif
