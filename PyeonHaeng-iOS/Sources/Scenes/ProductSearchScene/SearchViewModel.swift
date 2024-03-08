002a26255bac50d1aab3a91aaf9402a80491e6c0
EungCheol Kim <wannabeeung@gmail.com>
2024-03-08 18:01:15 +0900

    🔥 무한스크롤 제한으로 인한 관련 프로퍼티 삭제 및 검색 결과 없음을 나타내주는 프로퍼티 정의

--- PyeonHaeng-iOS/Sources/Scenes/ProductSearchScene/SearchViewModel.swift
+++ PyeonHaeng-iOS/Sources/Scenes/ProductSearchScene/SearchViewModel.swift
@@ -14,7 +14,6 @@ import SearchAPI
 
 enum SearchAction {
   case textChanged(String)
-  case loadMoreProducts
 }
 
 // MARK: - SearchState
@@ -24,11 +23,7 @@ struct SearchState {
   var products = [ConvenienceStore: [SearchProduct]]()
   var offset = 0
   var hasMore = false
-
-  /// 화면 중앙에 표시되는 로딩 인디케이터
-  var isLoading = false
-  /// 리스트 맨 하단에 표시되는 로딩 인디케이터
-  var isMoreLoading = false
+  var isNothing = false
 }
 
 // MARK: - SearchViewModelRepresentable
@@ -64,6 +59,7 @@ final class SearchViewModel: SearchViewModelRepresentable {
     }
   }
 
+  @MainActor
   private func handle(_ action: SearchAction) async {
     // 비동기 함수를 실행합니다. 만약 오류가 발생하면 Log로 값을 확인할 수 있습니다.
     func performAsyncAction(_ action: () async throws -> Void) async {
@@ -79,33 +75,25 @@ final class SearchViewModel: SearchViewModelRepresentable {
       state.currentText = text
       state.offset = 0
       await performAsyncAction {
-        try await fetchSearchList(isReplace: true)
-      }
-
-    case .loadMoreProducts:
-      await performAsyncAction {
-        try await fetchSearchList(isReplace: false)
+        try await fetchSearchList()
       }
     }
   }
 
-  private func fetchSearchList(isReplace: Bool) async throws {
+  @MainActor
+  private func fetchSearchList() async throws {
     let request = SearchProductRequest(
       name: state.currentText,
       order: .normal,
-      pageSize: 20,
+      pageSize: 50,
       offset: state.offset
     )
 
     let paginatedModel = try await service.fetchSearchList(request: request)
-
+    let results = Dictionary(grouping: paginatedModel.results, by: { $0.convenienceStore })
     state.hasMore = paginatedModel.hasMore
     state.offset += 1
-
-    let results = Dictionary(grouping: paginatedModel.results, by: { $0.convenienceStore })
-
-    if isReplace {
-      state.products = results
-    }
+    state.products = results
+    state.isNothing = results.isEmpty
   }
 }
