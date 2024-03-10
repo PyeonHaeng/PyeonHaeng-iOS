1015c1526182fed1b9ac52c793d8363b8ac2abe9
SeungHyun Hong <whi7ehyun@gmail.com>
2024-03-10 16:06:52 +0900

    :sparkles: Implement `MailSheetView` for email sending feature
    - Implemented `MailSheetView` to present a modal view for sending emails upon button press

--- PyeonHaeng-iOS/Sources/Scenes/SettingsScene/Mail/MailSheetView.swift
+++ PyeonHaeng-iOS/Sources/Scenes/SettingsScene/Mail/MailSheetView.swift
@@ -7,3 +7,40 @@
 
 import MessageUI
 import SwiftUI
+
+// MARK: - MailSheetView
+
+/// 메일 보내기 뷰를 구현하는 구조체
+struct MailSheetView: UIViewControllerRepresentable {
+  @Environment(\.presentationMode) var presentationMode
+  var subject: String
+  var recipients: [String]
+  var messageBody: String
+
+  func makeUIViewController(context: Context) -> MFMailComposeViewController {
+    let mail = MFMailComposeViewController()
+    mail.mailComposeDelegate = context.coordinator
+    mail.setSubject(subject)
+    mail.setToRecipients(recipients)
+    mail.setMessageBody(messageBody, isHTML: false)
+    return mail
+  }
+
+  func updateUIViewController(_: MFMailComposeViewController, context _: Context) {}
+
+  final class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+    var parent: MailSheetView
+
+    init(_ parent: MailSheetView) {
+      self.parent = parent
+    }
+
+    func mailComposeController(_: MFMailComposeViewController, didFinishWith _: MFMailComposeResult, error _: Error?) {
+      parent.presentationMode.wrappedValue.dismiss()
+    }
+  }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+}
