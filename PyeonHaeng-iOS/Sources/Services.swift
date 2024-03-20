b89a2b026d700b2d4a32c226a0d9dbc1b2779c12
EungCheol Kim <wannabeeung@gmail.com>
2024-03-20 22:18:42 +0900

    ⚡️ Add search, productinfo service in services

--- PyeonHaeng-iOS/Sources/Services.swift
+++ PyeonHaeng-iOS/Sources/Services.swift
@@ -11,39 +11,71 @@ import HomeAPISupport
 import Network
 import NoticeAPI
 import NoticeAPISupport
+import ProductInfoAPI
+import ProductInfoAPISupport
+import SearchAPI
+import SearchAPISupport
 
 // MARK: - Services
 
 struct Services {
   let homeService: HomeServiceRepresentable
   let noticeService: NoticeServiceRepresentable
-
-  init() {
+  let productInfoService: ProductInfoServiceRepresentable
+  let searchService: SearchServiceRepresentable
+  
+  init(productId: Int = 0) {
     let homeNetworking: Networking = {
       let configuration: URLSessionConfiguration
       #if DEBUG
-        configuration = .ephemeral
-        configuration.protocolClasses = [HomeURLProtocol.self]
+      configuration = .ephemeral
+      configuration.protocolClasses = [HomeURLProtocol.self]
       #else
-        configuration = .default
+      configuration = .default
       #endif
       let provider = NetworkProvider(session: URLSession(configuration: configuration))
       return provider
     }()
-
+    
     let noticeNetworking: Networking = {
       let configuration: URLSessionConfiguration
       #if DEBUG
-        configuration = .ephemeral
-        configuration.protocolClasses = [NoticeURLProtocol.self]
+      configuration = .ephemeral
+      configuration.protocolClasses = [NoticeURLProtocol.self]
       #else
-        configuration = .default
+      configuration = .default
       #endif
       let provider = NetworkProvider(session: URLSession(configuration: configuration))
       return provider
     }()
-
+    
+    let productInfoNetworking: Networking = {
+      let configuration: URLSessionConfiguration
+      #if DEBUG
+      configuration = .ephemeral
+      configuration.protocolClasses = [ProductInfoURLProtocol.self]
+      #else
+      configuration = .default
+      #endif
+      let provider = NetworkProvider(session: URLSession(configuration: configuration))
+      return provider
+    }()
+    
+    let searchNetworking: Networking = {
+      let configuration: URLSessionConfiguration
+      #if DEBUG
+      configuration = .ephemeral
+      configuration.protocolClasses = [SearchURLProtocol.self]
+      #else
+      configuration = .default
+      #endif
+      let provider = NetworkProvider(session: URLSession(configuration: configuration))
+      return provider
+    }()
+    
     homeService = HomeService(network: homeNetworking)
     noticeService = NoticeService(network: noticeNetworking)
+    productInfoService = ProductInfoService(productID: productId, network: productInfoNetworking)
+    searchService = SearchService(network: searchNetworking)
   }
 }
