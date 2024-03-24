e463df379844593b474902e1c17ede3859785bea
SeungHyun Hong <whi7ehyun@gmail.com>
2024-03-24 18:05:00 +0900

    ✨ Add NoticeDetailView and NoticeDetailViewModel
    ℹ️ Add NoticeDetailView and NoticeDetailViewModel files to display detailed notice information, using services provided by the container.

---
+++ PyeonHaeng-iOS/Sources/Scenes/SettingsScene/Notice/View/NoticeDetailView.swift
@@ -0,0 +1,29 @@
+//
+//  NoticeDetailView.swift
+//  PyeonHaeng-iOS
+//
+//  Created by 홍승현 on 3/24/24.
+//
+
+import DesignSystem
+import SwiftUI
+
+struct NoticeDetailView<ViewModel: NoticeDetailViewModelRepresentable>: View {
+  @StateObject private var viewModel: ViewModel
+
+  init(viewModel: @autoclosure @escaping () -> ViewModel) {
+    _viewModel = .init(wrappedValue: viewModel())
+  }
+
+  var body: some View {
+    if let context = viewModel.state.notice?.context {
+      MarkdownView(content: context)
+    } else {
+      ProgressView()
+        .progressViewStyle(.circular)
+        .onAppear {
+          viewModel.trigger(.fetchNoticeDetail)
+        }
+    }
+  }
+}
---
+++ PyeonHaeng-iOS/Sources/Scenes/SettingsScene/Notice/ViewModel/NoticeDetailViewModel.swift
@@ -0,0 +1,76 @@
+//
+//  NoticeDetailViewModel.swift
+//  PyeonHaeng-iOS
+//
+//  Created by 홍승현 on 3/24/24.
+//
+
+import Entity
+import Foundation
+import Log
+import NoticeAPI
+
+// MARK: - NoticeAction
+
+enum NoticeDetailAction {
+  case fetchNoticeDetail
+}
+
+// MARK: - NoticeState
+
+struct NoticeDetailState {
+  var notice: NoticeDetail?
+}
+
+// MARK: - NoticeViewModelRepresentable
+
+@MainActor
+protocol NoticeDetailViewModelRepresentable: ObservableObject {
+  var state: NoticeDetailState { get }
+  func trigger(_ action: NoticeDetailAction)
+}
+
+// MARK: - NoticeViewModel
+
+final class NoticeDetailViewModel: NoticeDetailViewModelRepresentable {
+
+  // MARK: Properties
+
+  private let service: NoticeServiceRepresentable
+  private let noticeID: Int
+
+  @Published private(set) var state: NoticeDetailState = .init()
+
+  // MARK: Initializations
+
+  init(service: NoticeServiceRepresentable, noticeID: Int) {
+    self.service = service
+    self.noticeID = noticeID
+  }
+
+  // MARK: Public Methods
+
+  func trigger(_ action: NoticeDetailAction) {
+    Task {
+      await handle(action)
+    }
+  }
+
+  // MARK: Helper Methods
+
+  private func handle(_ action: NoticeDetailAction) async {
+    do {
+      switch action {
+      case .fetchNoticeDetail:
+        try await fetchNoticeDetail()
+      }
+    } catch {
+      Log.make(with: .viewModel)?.error("\(String(describing: error))")
+    }
+  }
+
+  /// 공지 목록을 가져옵니다.
+  private func fetchNoticeDetail() async throws {
+    state.notice = try await service.fetchNoticeDetail(id: noticeID)
+  }
+}
