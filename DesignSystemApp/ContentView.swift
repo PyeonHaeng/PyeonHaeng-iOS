d72b19bf434a5a9aad66cd274c04972b25485079
SeungHyun Hong <whi7ehyun@gmail.com>
2024-03-24 14:53:21 +0900

    🚀 Add initial Xcode project setup for DesignSystemApp

---
+++ DesignSystemApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignSystemApp
+//
+//  Created by 홍승현 on 3/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignSystemApp/DesignSystemAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSystemAppApp.swift
+//  DesignSystemApp
+//
+//  Created by 홍승현 on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignSystemAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
