175c7161acaf7defba6d1dfaa48e4d5aed5f2e9c
SeungHyun Hong <whi7ehyun@gmail.com>
2024-03-25 15:22:51 +0900

    ✨ Add NetworkMonitor for monitoring connection status in App.

--- Core/Sources/NetworkMonitor/NetworkMonitor.swift
+++ Core/Sources/NetworkMonitor/NetworkMonitor.swift
@@ -13,19 +13,12 @@ import Network
 public final class NetworkMonitor: ObservableObject {
   private let monitor = NWPathMonitor()
   private let queue = DispatchQueue(label: .networkMonitor)
-  @Published public var isActive = false
-  @Published public var isExpensive = false
-  @Published public var isConstrained = false
-  @Published public var connectionType = NWInterface.InterfaceType.other
+  @Published public var isSatisfied = false
 
   public init() {
     monitor.pathUpdateHandler = { path in
       DispatchQueue.main.async {
-        self.isActive = path.status == .satisfied
-        self.isExpensive = path.isExpensive
-        self.isConstrained = path.isConstrained
-        let connectionType: [NWInterface.InterfaceType] = [.cellular, .wifi, .wiredEthernet]
-        self.connectionType = connectionType.first(where: path.usesInterfaceType(_:)) ?? .other
+        self.isSatisfied = path.status == .satisfied
       }
     }
     monitor.start(queue: queue)
