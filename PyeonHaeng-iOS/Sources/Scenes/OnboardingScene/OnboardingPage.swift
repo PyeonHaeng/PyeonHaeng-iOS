1016dff52fc0e1ca83713aac6dcd37ac3c3fb590
SeungHyun Hong <whi7ehyun@gmail.com>
2024-04-02 15:05:27 +0900

    ✨ Add multilingual support for new promotional content.

--- PyeonHaeng-iOS/Sources/Scenes/OnboardingScene/OnboardingPage.swift
+++ PyeonHaeng-iOS/Sources/Scenes/OnboardingScene/OnboardingPage.swift
@@ -10,21 +10,23 @@ import SwiftUI
 
 struct OnboardingPage: Identifiable {
   let id = UUID()
-  let title: String
-  let body: String
+  let title: LocalizedStringKey
+  let body: LocalizedStringKey
   let image: Image
   let tag: Int
 
   static let pages: [OnboardingPage] = [
     OnboardingPage(
-      title: "하나사면 하나 더",
-      body: "다양한 편의점의 1+1, 2+1 행사 제품 정보로\n알뜰하고 합리적으로 소비할수 있어요.",
+      title: "Buy one, get one free",
+      body: "Discover 1+1, 2+1 promotional product information from various convenience stores to consume smartly and economically.",
       image: .onboarding1,
       tag: 0
     ),
     OnboardingPage(
-      title: "수많은 혜택을 한곳에서",
-      body: "세븐일레븐, CU, 이마트 24, GS 25, 미니스톱의 수많은 행사정보를 ‘편행’한곳에서 만나보세요.",
+      title: "Numerous benefits in one place",
+      body: """
+      Find the myriad of promotional information from 7-Eleven, CU, emart24, GS25, MiniStop all in one place with ’`Pyeonhaeng`’.
+      """,
       image: .onboarding2,
       tag: 1
     ),
